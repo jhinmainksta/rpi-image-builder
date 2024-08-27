9870591768a6ac2c3f29b9bbc34c602731271fcd
jhinmainksta <gtslnok@bk.ru>
2024-08-28 10:14:42 +1100

    Update dronelibs.sh

--- dronelibs.sh
+++ dronelibs.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
+echo dronelibs started
+
+python3 -V
 pip3 -V || { sudo apt-get install -y python3-pip; pip3 -V; }
 sudo apt-get remove modemmanager
 
 if ! [ -d drone ]; then
-	python -m venv /usr/local/bin/drone
+	{ sudo python -m venv /usr/local/bin/drone; echo repo created; }
+else
+	echo  repo already exists 
 fi
+cd /usr/local/bin/drone/bin
 
-source drone/bin/activate
-
-pip show pymavlink || { pip3 install pymavlink; pip show pymavlink; }
-pip show dronekit || { pip3 install dronekit; pip show dronekit; }
-pip show mavproxy || { pip3 install mavproxy; pip show mavproxy; }
+sudo pip show pymavlink || { sudo /usr/local/bin/drone/bin/pip install pymavlink; pip show pymavlink; }
+sudo pip show dronekit || { sudo /usr/local/bin/drone/bin/pip install dronekit; pip show dronekit; }
+sudo pip show mavproxy || { sudo /usr/local/bin/drone/bin/pip install mavproxy; pip show mavproxy; }
 
 mavproxy.py --master=/dev/ttyS0 --baudrate 57600 --aircraft MyCopter
 . ~/.bashrc
