b2da5fa2352d3bd0bedf0b96049c284b9b2a10d1
jhinmainksta <gtslnok@bk.ru>
2024-08-27 14:08:31 +1100

    plugin script wich create venv and load required packages

---
+++ dronelibs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+pip3 -V || { sudo apt-get install -y python3-pip; pip3 -V; }
+sudo apt-get remove modemmanager
+
+if ! [ -d drone ]; then
+	python -m venv drone
+fi
+
+source drone/bin/activate
+
+pip show pymavlink || { pip3 install pymavlink; pip show pymavlink; }
+pip show dronekit || { pip3 install dronekit; pip show dronekit; }
+pip show mavproxy || { pip3 install mavproxy; pip showmavproxy; }
+
+mavproxy.py --master=/dev/ttyS0 --baudrate 57600 --aircraft MyCopter
+. ~/.bashrc
