5d9c0c8d3535cdee4f081ea680353e8af4e5ef7e
jhinmainksta <gtslnok@bk.ru>
2024-08-28 11:07:41 +1100

    rc.local fixes

--- dronelibs.sh
+++ dronelibs.sh
@@ -4,18 +4,18 @@ echo dronelibs started
 
 python3 -V
 pip3 -V || { sudo apt-get install -y python3-pip; pip3 -V; }
-sudo apt-get remove modemmanager
+sudo apt-get remove -y modemmanager
 
 if ! [ -d drone ]; then
 	{ sudo python -m venv /usr/local/bin/drone; echo repo created; }
 else
-	{ echo repo already exists; exit 0; } 
+	echo  repo already exists 
 fi
 cd /usr/local/bin/drone/bin
 
-sudo pip show pymavlink || { sudo /usr/local/bin/drone/bin/pip install pymavlink; pip show pymavlink; }
-sudo pip show dronekit || { sudo /usr/local/bin/drone/bin/pip install dronekit; pip show dronekit; }
-sudo pip show mavproxy || { sudo /usr/local/bin/drone/bin/pip install mavproxy; pip show mavproxy; }
+sudo /usr/local/bin/drone/bin/pip install pymavlink
+sudo /usr/local/bin/drone/bin/pip install dronekit
+sudo /usr/local/bin/drone/bin/pip install mavproxy
 
 mavproxy.py --master=/dev/ttyS0 --baudrate 57600 --aircraft MyCopter
 . ~/.bashrc
