bf80a90fa9904a9e6c577ff05b71079057fe9810
jhinmainksta <gtslnok@bk.ru>
2024-09-03 16:44:16 +1100

    added script

---
+++ dronelibs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ -d /usr/drone ] ; then	
+	exit 0
+fi
+
+python3 -V || sudo apt-get install -y python3
+pip3 -V || sudo apt-get install -y python3-pip
+
+ssudo python -m venv /usr/drone
+
+sudo /usr/drone/bin/pip install pymavlink
+sudo /usr/drone/bin/pip install dronekit
+sudo /usr/drone/bin/pip install mavproxy
