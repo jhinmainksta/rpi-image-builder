20c029b06bcdee428ec006f3778c99d1c8361a50
jhinmainksta <gtslnok@bk.ru>
2024-09-09 17:53:55 +1100

    idiot?hto?nu_ti

--- scripts/gpioenable.sh
+++ scripts/gpioenable.sh
@@ -10,7 +10,7 @@ echo gpio not created
 
 sudo sed -i "s|#dtparam=i2c_arm=on|dtparam=i2c_arm=on|" /boot/firmware/config.txt || { echo unable to fix config.txt; exit 1; }
 sudo sh -c "echo enable_uart=1>> /boot/firmware/config.txt" || { echo unable to fix config.txt; exit 1;}
-sudo sed -i "s|console=serial0,115200  ||" /boot/firmware/cmdline.txt || { echo unable to fix cmdline.txt; exit 1; }
+sudo sed -i "s|console=serial0,115200 ||" /boot/firmware/cmdline.txt || { echo unable to fix cmdline.txt; exit 1; }
 
 sudo touch /usr/local/bin/gpio || { echo unable to touch gpio; sleep 2.5; exit 1; }
 echo gpio created
